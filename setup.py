a483e7d5a9e28cce11fea95e29192f84ec10db26
agent87 <arnauldkayonga1@gmail.com>
2020-03-31 12:22:40 +0200

    requirements file

--- setup.py
+++ setup.py
@@ -1,1 +1,78 @@
-import pip 
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requirements = open("requirements.txt").readlines()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='shora',  # Required
+    version='1.1.2',  # Required
+    description='Shora AI Analyst Fellowship',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/Shora-AI-Analyst-Fellowship',  # Optional
+    author='SHORA AI Analyst Fellowship',  # Optional
+    author_email=['arnauldkayonga1@gmail.com'],  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Data Scientist',
+        'Topic :: Software Development :: Build Data Analysis Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='sample setuptools development data-science visualisation',  # Optional
+
+    packages=find_packages(where='src'),  # Required
+    python_requires='!=3.4.*, !=3.5.*, !=3.6.*, !=3.7.*, !=3.8.*, <4',
+
+    install_requires= requirements,  # Optional
+
+#    extras_require={  # Optional
+#        'dev': ['check-manifest'], 
+#        'test': ['coverage'],
+#    },
+
+ #   package_data={  # Optional
+ #       'sample': ['package_data.dat'],
+ #   },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
+    },
+)
